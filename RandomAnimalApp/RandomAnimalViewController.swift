2fb1635c3b4ea7781332a8ad38aa584402a047a8
Teodoro <teo.calle.lara@gmail.com>
2022-11-26 17:53:35 -0500

    ...

--- RandomAnimalApp/RandomAnimalViewController.swift
+++ RandomAnimalApp/RandomAnimalViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  RandomAnimalViewController.swift
 //  RandomAnimalApp
 //
 //  Created by TEO on 26/11/22.
@@ -7,11 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class RandomAnimalViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
 
